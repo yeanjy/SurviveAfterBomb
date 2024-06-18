8d791baae20b811a2a7b6d31c5df9dd8ce9345ed
Yean Jy Chen <yeanchen3@gmail.com>
2024-06-18 10:53:50 -0300

    update

--- include/variables.hpp
+++ include/variables.hpp
@@ -1,10 +1,7 @@
 #include <random>
 #include <string>
-#include "json.hpp" 
-using json = nlohmann::json;
 
 extern std::string openText;
 extern std::string endText;
 extern std::random_device rd;
 extern std::mt19937 gen;
-extern std::string jsonString;
